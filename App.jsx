62a2b38c8cbce2b719e596c87e65781732c53ecb
EmreSafaCelik <safaemrecelik@gmail.com>
2023-10-12 15:28:17 +0300

    forking from here for solution

--- App.jsx
+++ App.jsx
@@ -1,67 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button, Text, StyleSheet, Alert } from 'react-native';
-import { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
+import React from 'react'
+import { Text } from 'react-native'
 
-const MMKV = new MMKVLoader().initialize();
 
-const MMKVStorageTest = () => {
-    const [inputValue, setInputValue] = useState('');
-    const [storedValue, setStoredValue] = useMMKVStorage('test_key', MMKV);
-
-    const saveValueToStorage = () => {
-        try {
-            MMKV.setString('test_key', inputValue);
-            Alert.alert("Success!", "Value saved to MMKV storage.");
-        } catch (error) {
-            Alert.alert("Error", "There was an error saving the value.");
-        }
-    };
-
-    const loadValueFromStorage = () => {
-        try {
-            const value = MMKV.getString('test_key');
-            if (value) {
-                setStoredValue(value);
-            } else {
-                Alert.alert("Info", "No value found in MMKV storage.");
-            }
-        } catch (error) {
-            Alert.alert("Error", "There was an error loading the value.");
-        }
-    };
-
+const App = () => {
     return (
-        <View style={styles.container}>
-            <TextInput 
-                style={styles.input}
-                value={inputValue}
-                onChangeText={setInputValue}
-                placeholder="Type something..."
-            />
-            <Button title="Save" onPress={saveValueToStorage} />
-            <Button title="Load" onPress={loadValueFromStorage} />
-            <Text style={styles.storedText}>Loaded from MMKV storage: {storedValue}</Text>
-        </View>
-    );
-};
+        <Text> Forking from here </Text>
+    )
+}
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 16,
-    },
-    input: {
-        width: '100%',
-        padding: 10,
-        borderWidth: 1,
-        borderColor: 'gray',
-        marginBottom: 20,
-    },
-    storedText: {
-        marginTop: 20,
-    }
-});
-
-export default MMKVStorageTest;
+export default App;
