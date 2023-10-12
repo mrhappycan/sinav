41fbf202d39a0a8e49ba85d0dbcdb4fe0041e45c
EmreSafaCelik <safaemrecelik@gmail.com>
2023-10-12 15:16:47 +0300

    package testing
    checked if asyncstorage and react navigation works with new installations

--- App.tsx
+++ App.tsx
@@ -1,160 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, StyleSheet, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TouchableOpacity,
-} from 'react-native';
+const AsyncStorageTest = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [storedValue, setStoredValue] = useState('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    const saveValueToStorage = async () => {
+        try {
+            await AsyncStorage.setItem('test_key', inputValue);
+            Alert.alert("Success!", "Value saved to AsyncStorage.");
+        } catch (error) {
+            Alert.alert("Error", "There was an error saving the value.");
+        }
+    };
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+    const loadValueFromStorage = async () => {
+        try {
+            const value = await AsyncStorage.getItem('test_key');
+            if (value !== null) {
+                setStoredValue(value);
+            } else {
+                Alert.alert("Info", "No value found in AsyncStorage.");
+            }
+        } catch (error) {
+            Alert.alert("Error", "There was an error loading the value.");
+        }
+    };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  function Home({navigation}: {navigation: any}): JSX.Element {
     return (
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('NewHome')
-              }}
-            >
-              <Section title="Learn More">
-                Read the docs to discover what to do next:
-              </Section>
-            </TouchableOpacity>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    )
-  }
-
-  function NewHome(): JSX.Element {
-    return (
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <Text>New Home</Text>
-      </SafeAreaView>
-    )
-  }
-
-  const Stack = createNativeStackNavigator();
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          component={Home}
-          name='Home'
-        />
-
-        <Stack.Screen
-          component={NewHome}
-          name='NewHome'
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+        <View style={styles.container}>
+            <TextInput 
+                style={styles.input}
+                value={inputValue}
+                onChangeText={setInputValue}
+                placeholder="Type something..."
+            />
+            <Button title="Save" onPress={saveValueToStorage} />
+            <Button title="Load" onPress={loadValueFromStorage} />
+            <Text style={styles.storedText}>Loaded from AsyncStorage: {storedValue}</Text>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+    },
+    input: {
+        width: '100%',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        marginBottom: 20,
+    },
+    storedText: {
+        marginTop: 20,
+    }
 });
 
-export default App;
+export default AsyncStorageTest;
